8d8cfc718ca1d159a6240a46a5b9c4af2410f00b
Andrew Wagner <scalliwags65@gmail.com>
2016-11-12 15:30:49 -0500

    working student endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,15 @@ import { version } from '../../package.json'
 import { Router } from 'express'
 import facets from './facets'
 import placements from './placements'
-
+import students from './students'
 export default ({ config, db }) => {
 	let api = Router()
 
 	// mount the facets resource
 	api.use('/facets', facets({ config, db }))
 	api.use('/placements', placements({ config, db }))
+	api.use('/students', students({ config, db }))
+
 	
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
---
+++ src/api/students.js
@@ -0,0 +1,68 @@
+import resource from 'resource-router-middleware'
+import * as studentsDao from '../daos/students-dao'
+
+export default ({ config, db }) => resource({
+
+	/** Property name to store preloaded entity on `request`. */
+	id: 'studentID',
+
+	/** GET /:id - Return a given student */
+	read(req, res) {
+		const studentID = parseInt(req.params.studentID)
+		if (studentID >= 0) {
+			studentsDao.getStudent(studentID)
+				.then(student => {
+					res.status(200).json(student)
+				})
+				.catch(err => {
+					console.error(err)
+					res.sendStatus(404)
+				})
+		} else {
+			res.sendStatus(404)
+		}
+	},
+	/** POST /:id - Create a new student with id  */
+	create(req, res) {
+		studentsDao.createStudent(req.body.student)
+				.then(student => {
+					res.sendStatus(200)
+				})
+				.catch(err => {
+					console.error(err)
+					res.sendStatus(404)
+				})
+	},
+	/** PUT /:id - update a students info, this expects a json object of all of the students data */
+	update(req, res) {
+		const studentID = req.body.student.id
+		if (studentID >= 0) {
+			studentsDao.updateStudent(req.body.student)
+				.then(student => {
+					res.sendStatus(200)
+				})
+				.catch(err => {
+					console.error(err)
+					res.sendStatus(404)
+				})
+		} else {
+			res.sendStatus(404)
+		} 
+	},
+	/** DELETE /:id - Delete a given student based on their id */
+	delete(req, res) {
+		const studentID = parseInt(req.params.studentID)
+		if (studentID > 0) {
+		studentsDao.deleteStudent(studentID)
+			.then(student => {
+				res.sendStatus(200)
+			})
+			.catch(err => {
+				console.error(err)
+				res.sendStatus(404)
+			})
+		} else {
+			res.sendStatus(404)
+		}
+	} 
+})
