198c39bad4ffff2e1251e4450337dcec5674c142
James Clements <jamesvclements@Jamess-MacBook-Pro.local>
2016-11-14 23:58:03 -0500

    formatting

--- src/api/students.js
+++ src/api/students.js
@@ -1,5 +1,5 @@
 import resource from 'resource-router-middleware'
-import * as studentsDao from '../daos/students-dao'
+import * as studentsDao from '../daos/student-dao'
 
 export default ({ config, db }) => resource({
 
@@ -22,47 +22,50 @@ export default ({ config, db }) => resource({
 			res.sendStatus(404)
 		}
 	},
-	/** POST /:id - Create a new student with id  */
+
+	/** POST - Create a new student */
 	create(req, res) {
 		studentsDao.createStudent(req.body.student)
-				.then(student => {
-					res.sendStatus(200)
-				})
-				.catch(err => {
-					console.error(err)
-					res.sendStatus(404)
-				})
-	},
-	/** PUT /:id - update a students info, this expects a json object of all of the students data */
-	update(req, res) {
-		const studentID = req.body.student.id
-		if (studentID >= 0) {
-			studentsDao.updateStudent(req.body.student)
-				.then(student => {
-					res.sendStatus(200)
-				})
-				.catch(err => {
-					console.error(err)
-					res.sendStatus(404)
-				})
-		} else {
-			res.sendStatus(404)
-		} 
-	},
-	/** DELETE /:id - Delete a given student based on their id */
-	delete(req, res) {
-		const studentID = parseInt(req.params.studentID)
-		if (studentID > 0) {
-		studentsDao.deleteStudent(studentID)
-			.then(student => {
+			.then(() => {
 				res.sendStatus(200)
 			})
 			.catch(err => {
 				console.error(err)
 				res.sendStatus(404)
 			})
+	},
+
+	/** PUT /:id - update a students info, this expects a json object of all of the students data */
+	update(req, res) {
+		const studentID = req.body.student.id
+		if (studentID >= 0) {
+			studentsDao.updateStudent(req.body.student)
+				.then(() => {
+					res.sendStatus(200)
+				})
+				.catch(err => {
+					console.error(err)
+					res.sendStatus(404)
+				})
 		} else {
 			res.sendStatus(404)
 		}
-	} 
+	},
+
+	/** DELETE /:id - Delete a given student based on their id */
+	delete(req, res) {
+		const studentID = parseInt(req.params.studentID)
+		if (studentID > 0) {
+			studentsDao.deleteStudent(studentID)
+				.then(() => {
+					res.sendStatus(200)
+				})
+				.catch(err => {
+					console.error(err)
+					res.sendStatus(404)
+				})
+		} else {
+			res.sendStatus(404)
+		}
+	}
 })
