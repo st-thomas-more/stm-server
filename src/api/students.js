f1ca9645a172197ce7079601cec94480d7780b18
keh217 <keh217@lehigh.edu>
2016-11-21 00:52:26 -0500

    Student dao (#27)
    * Replaced GET student with an ID
    
    * Added CUD of CRUD for student API
    
    * Added an API call to return a list of students in the school
    
    * Modified the DB columns to match the already used fields' names
    
    * Added dial4 field to student GET
    
    * Added more fields to return in Student GET
    
    * formatting

--- src/api/students.js
+++ src/api/students.js
@@ -8,7 +8,7 @@ export default ({ config, db }) => resource({
 
 	/** GET / - Return all students */
 	list(req, res) {
-		studentDAO.getStudents()
+		studentDAO.getStudents(db)
 			.then(students => {
 				res.status(200).json(students)
 			})
@@ -20,7 +20,7 @@ export default ({ config, db }) => resource({
 	
 	/** GET /:studentID - Return a given student */
 	read(req, res) {
-		studentDAO.getStudent(req.params.studentID)
+		studentDAO.getStudent(req.params.studentID, db)
 			.then(student => {
 				res.status(200).json(student)
 			})
@@ -32,7 +32,7 @@ export default ({ config, db }) => resource({
 
 	/** POST - Create a new student */
 	create(req, res) {
-		studentDAO.createStudent(req.body.student)
+		studentDAO.createStudent(req.body.student, db)
 			.then(() => {
 				res.sendStatus(201)
 			})
@@ -44,7 +44,7 @@ export default ({ config, db }) => resource({
 
 	/** PUT / - Update a students info, this expects a json object of all of the students data */
 	update(req, res) {
-		studentDAO.updateStudent(req.body.student)
+		studentDAO.updateStudent(req.body.student, db)
 			.then(() => {
 				res.sendStatus(200)
 			})
@@ -56,7 +56,7 @@ export default ({ config, db }) => resource({
 
 	/** DELETE /:studentID - Delete a given student based on their id */
 	delete(req, res) {
-		studentDAO.deleteStudent(req.params.studentID)
+		studentDAO.deleteStudent(req.params.studentID, db)
 			.then(() => {
 				res.sendStatus(204)
 			})
--- src/db.js
+++ src/db.js
@@ -1,4 +1,13 @@
+import mysql from 'mysql'
+import config from './config.json'
+
 export default callback => {
 	// connect to a database if needed, the pass it to `callback`:
-	callback()
+	callback(mysql.createConnection({
+        host: config.mysql_host,
+        user: config.mysql_user,
+        password: config.mysql_password,
+        database: 'STM_DB',
+        multipleStatements: true
+    }))
 }
