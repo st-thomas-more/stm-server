6d0291ec9facda7cfb0e5ae9b3d2a0a8a1727156
Kento Hirakawa <keh217@lehigh.edu>
2016-11-20 22:20:23 -0500

    Modified the DB columns to match the already used fields' names

--- src/daos/student-dao.js
+++ src/daos/student-dao.js
@@ -2,8 +2,9 @@ import studentRaw from '../mock-data/raw/student-raw'
 
 export function getStudents(db) {
   return new Promise((resolve, reject) => {
-    db.query('SELECT `id`, `sex`, `firstName`, `lastName`, `classroomBehavior` AS `behavior`, `facStudent` AS `facultyStudent`, `newStudent`, `medicalConcern`, ' +
-             '`HMP` AS `hmp`, `workHabits` AS `workEthic`, `mathBenchmark` AS `mathBench`, `DRA` AS `dra`, `aspDate` AS `asp`  FROM `student` NATURAL JOIN `ydsd`',
+    db.query('SELECT `id`, `sex`, `firstName`, `lastName`, `behavior`, `facultyStudent`, `newStudent`, `medicalConcern`, ' +
+             '`hmp`, `workEthic`, `mathBench`, `dra`, `asp`, `elaTotal`, `mathTotal`, `cogAT` ' +
+             'FROM `student` NATURAL JOIN `ydsd`',
       function (err, entities) {
         if (err) {
           reject(err)
@@ -16,8 +17,8 @@ export function getStudents(db) {
 
 export function getStudent(studentID, db) {
   return new Promise((resolve, reject) => {
-    db.query('SELECT `id`, `sex`, `firstName`, `lastName`, `classroomBehavior` AS `behavior`, `facStudent` AS `facultyStudent`, `newStudent`, `medicalConcern`, ' +
-             '`HMP` AS `hmp`, `workHabits` AS `workEthic`, `mathBenchmark` AS `mathBench`, `DRA` AS `dra`, `aspDate` AS `asp` ' +
+    db.query('SELECT `id`, `sex`, `firstName`, `lastName`, `behavior`, `facultyStudent`, `newStudent`, `medicalConcern`, ' +
+             '`hmp`, `workEthic`, `mathBench`, `dra`, `asp`, `elaTotal`, `mathTotal`, `cogAT` ' +
              'FROM `student` NATURAL JOIN `ydsd` WHERE `id` = ?', studentID,
       function (err, entities){
         if (err) {
@@ -57,7 +58,7 @@ export function updateStudent(student, db) {
 
 export function deleteStudent(studentID, db) {
   return new Promise((resolve, reject) => {
-    db.query('DELETE FROM `student` WHERE `id` = ?', studentID, function (err) {
+    db.query('DELETE FROM `student` WHERE `id` = ?; DELETE FROM `ydsd` WHERE `id` = ?', [studentID, studentID], function (err) {
       if (err) {
         reject(err)
       } else {
@@ -72,11 +73,11 @@ export function createStudent(student, db){
     db.query('INSERT INTO `student` (`id`, `lastName`, `firstName`, `sex`, `dob`, `dial4`) VALUES(?, ?, ?, ?, ?,?);' +
              'INSERT INTO `ydsd` VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);',
       [student.id, student.lastName, student.firstName, student.sex, student.dob, student.dial4,
-      student.id, student.year, student.comments, student.homeroomTeacher, student.aspDate, student.nextMeetingSch,
+      student.id, student.year, student.comments, student.homeroomTeacher, student.asp, student.nextMeetingSch,
       student.advancedMath, student.speechLanguage, student.studentDevelopment, student.mathEnrichment, student.IUreadingServices,
-      student.IUmathServices, student.earobics, student.classroomBehavior, student.workHabits, student.youngestChild, student.onlyChild,
-      student.newStudent, student.medicalConcern, student.HMP, student.DRA, student.RAZ, student.WTW, student.iStation, student.mathBenchmark,
-      student.Dibels, student.CogAT, student.IOWA, student.ELA, student.ExtendedELA, student.mathTotal, student.facStudent], function (err) {
+      student.IUmathServices, student.earobics, student.behavior, student.workEthic, student.youngestChild, student.onlyChild,
+      student.newStudent, student.medicalConcern, student.hmp, student.dra, student.raz, student.wtw, student.iStation, student.mathBench,
+      student.Dibels, student.cogAT, student.IOWA, student.elaTotal, student.ExtendedELA, student.mathTotal, student.facultyStudent], function (err) {
       if (err) {
         reject(err)
       } else {
