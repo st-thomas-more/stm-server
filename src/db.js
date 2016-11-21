ef1026ff73d49cabc355a1679e8862fbb251f960
James Clements <jamesvclements@users.noreply.github.com>
2016-11-21 01:06:54 -0500

    resolves #30 (#31)

--- src/db.js
+++ src/db.js
@@ -1,12 +1,11 @@
 import mysql from 'mysql'
-import config from './config.json'
 
 export default callback => {
 	// connect to a database if needed, the pass it to `callback`:
 	callback(mysql.createConnection({
-        host: config.mysql_host,
-        user: config.mysql_user,
-        password: config.mysql_password,
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
         database: 'STM_DB',
         multipleStatements: true
     }))
