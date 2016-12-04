d365bf51ce8d73ac27628612aceb95a151edd6a7
Erik Outhwaite <eno217@lehigh.edu>
2016-12-04 15:00:49 -0500

    adding staff-dao

---
+++ src/daos/staff-dao.js
@@ -0,0 +1,25 @@
+export function updateGrade(emailID,gradeTeaching,db){
+	return new Promise((resolve, reject) => {
+		db.query("UPDATE staff set gradeTeaching = ? WHERE emailID = ?;",[gradeTeaching,emailID],
+             function (err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+        })
+        })
+}
+
+export function getGrade(emailID,db){
+	return new Promise((resolve, reject) => {
+		db.query("select * from staff WHERE emailID = ?;",emailID,
+             function (err, entities) {
+	        if (err) {
+	          reject(err)
+	        } else {
+	          resolve(entities[0].gradeTeaching)
+	        }
+	        })
+        })
+}
