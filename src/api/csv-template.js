092e4cac5fd00c6defc6df8d18f1f2cf05b4528b
James Clements <jamesvclements@dyn096082.cc.lehigh.edu>
2016-12-04 20:12:16 -0500

    fix lint

--- src/api/csv-template.js
+++ src/api/csv-template.js
@@ -2,21 +2,20 @@ import resource from 'resource-router-middleware'
 import { resolve } from 'path'
 export default ({ config, db }) => resource({
 
-
-	/** GET / - Return all students */
-	list(req, res) {
-		var options = {
-		    dotfiles: 'allow',
-		    headers: {
-		        'x-timestamp': Date.now(),
-		        'x-sent': true
-		    }
-		}
-		res.sendFile(resolve('src/services/upload/gradeCSVTemplate.csv'), options, function (err) {
-		    if (err) {
-		      console.log(err);
-		      res.status(err.status).send();
-		    }
-		});
-	},
+  /** GET / - Return all students */
+  list(req, res) {
+    var options = {
+      dotfiles: 'allow',
+      headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+      }
+    }
+    res.sendFile(resolve('src/services/upload/gradeCSVTemplate.csv'), options, function (err) {
+      if (err) {
+        console.log(err)
+        res.status(err.status).send()
+      }
+    })
+  },
 })
