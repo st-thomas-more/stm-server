7cbf00e3c85f793fa49111bb4fb389dd5762ab4b
Erik Outhwaite <eno217@lehigh.edu>
2016-12-05 20:21:23 -0500

    updated incrementDashYear in current-year-dao so that in the year dependent student data table all students with entries in the current year get their entries copied and the columns year and gradeEntering are incremented

--- src/daos/current-year-dao.js
+++ src/daos/current-year-dao.js
@@ -1,16 +1,34 @@
+//For this function we need to first update ydsd table so that gradeEntering is incremented and year is incremented, and then we can increment the year. Consider changing method name for this reason.
 export function incrementDashYear(db) {
   return new Promise((resolve, reject) => {
-    db.query('UPDATE currentDashboardYear set currentYear = currentYear + 1;'
-      , function (err) {
-        if (err) {
-          reject(err)
-        } else {
-          resolve()
-        }
-      })
-  })
-}
-
+	  getDashYear(db)
+	  .then(year => {
+		  db.query('Insert into ydsd( id, year,comments,homeroomTeacher,asp,nextMeetingSch,advancedMath,speechLanguage,studentDevelopment, mathEnrichment, IUreadingServices, IUmathServices,earobics, workEthic, youngestChild, onlyChild, newStudent, medicalConcern, hmp, dra, RAZ, WTW,iStation, mathBench, Dibels, cogAT, IOWA, elaTotal,ExtendedELA, mathTotal, facultyStudent, potentialDelay, behaviorObservation, selfHelp, socialEmotional, dial4, gradeEntering, ge) SELECT id, year + 1, comments,homeroomTeacher,asp,nextMeetingSch,advancedMath, speechLanguage, studentDevelopment, mathEnrichment, IUreadingServices, IUmathServices, earobics, workEthic, youngestChild, onlyChild, newStudent, medicalConcern, hmp, dra, RAZ, WTW, iStation, mathBench, Dibels, cogAT, IOWA, elaTotal,ExtendedELA, mathTotal, facultyStudent, potentialDelay, behaviorObservation, selfHelp, socialEmotional, dial4, gradeEntering + 1, ge from ydsd where year = ?;', year,
+			   function(err){
+			       if(err){
+				   console.log('part 1 of incrementDashYear failure')
+				   reject(err)
+			       }else{
+				   console.log('success @ part 1 of incremenetDashYear')
+				   db.query('UPDATE currentDashboardYear set currentYear = currentYear + 1;'
+					    , function (err) {
+						if (err) {
+						    reject(err)
+						} else {
+						    resolve()
+						}
+					    })
+			       }
+			   }
+			   )
+	      })
+	  
+	  .catch(err => {
+		  reject(err)
+	      })
+	      })
+      }
+      
 export function decrementDashYear(db) {
   return new Promise((resolve, reject) => {
     db.query('UPDATE currentDashboardYear set currentYear = currentYear - 1;'
