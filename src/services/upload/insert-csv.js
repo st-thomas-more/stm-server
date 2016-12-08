1104975c428a9f48beac50781b5eb4489ca2cb1d
Seth K Deal <skd216>
2016-12-07 19:52:05 -0500

    added import of utils to insert-csv

--- src/services/upload/insert-csv.js
+++ src/services/upload/insert-csv.js
@@ -1,6 +1,8 @@
 import * as fs from 'fs'
 import csvjson from 'csvjson'
 
+import * as utils from '../''/lib/utils.js'
+
 export default function insertCSV(filename, db) {
 
 	let data = fs.readFileSync(filename, { encoding: 'utf8' })
